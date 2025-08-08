789e562874f37ec917693e5e7888a91da9dfaa0c
Bryan Maynard <bsgbryan@gmail.com>
2025-08-07 23:21:13 -0500

    Implemented support for nested component rendering

--- lib/render.ts
+++ lib/render.ts
@@ -2,9 +2,15 @@ const fun = (
   element: JSX.Element,
   container: Element,
 ) => {
-  const dom = document.createElement(element.tagName)
+	const context: JSX.Element = typeof element.tagName === 'string' ?
+		element
+		:
+		// @ts-expect-error tagName is the function that produces the JSX.Element
+		element.tagName()
 
-  for (const [k, v] of Object.entries(element.attribs ?? {})) {
+	const dom = document.createElement(context.tagName)
+
+  for (const [k, v] of Object.entries(context.attribs ?? {})) {
     if (k === 'text') dom.appendChild(document.createTextNode(v as string))
     else {
       const n = document.createAttribute(k === 'clazz' ? 'class' : k)
@@ -13,7 +19,7 @@ const fun = (
     }
   }
 
-  for (const c of element.children ?? []) fun(c, dom)
+  for (const c of context.children ?? []) fun(c, dom)
 
   container.appendChild(dom)
 }
